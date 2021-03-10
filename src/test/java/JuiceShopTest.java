ddc54a0b498e0a7a215b3b925ad2e9e56d2bbd1d
Shwetahe <shwetahe@thoughtworks.com>
2021-03-10 12:54:43 +0530

    adding test

--- src/test/java/JuiceShopTest.java
+++ src/test/java/JuiceShopTest.java
@@ -1,3 +1,6 @@
+import datamodels.LoginData;
+import datamodels.RegisterCustomerData;
+import datamodels.SecurityQuestion;
 import net.minidev.json.JSONObject;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
@@ -5,15 +8,44 @@ import static io.restassured.RestAssured.*;
 public class JuiceShopTest extends BaseConfig {
     @Test
     public void loginToJuiceApp() {
-        JSONObject loginRequestBody = new JSONObject();
-        loginRequestBody.put("email", "shweta1@gmail.com");
-        loginRequestBody.put("password", "Password@123");
-
         given()
-                .body(loginRequestBody).
+                .body(loginDataBuilder()).
         when()
                 .post("rest/user/login").
         then()
-                .log().all();
+                .log().all()
+        .statusCode(200);
     }
+
+    @Test
+    public void register() {
+        given()
+                .body(registerCustomerDataBuilder()).
+        when()
+                .post("api/Users/").
+        then()
+                .log().all()
+        .statusCode(200);
+    }
+
+    private LoginData loginDataBuilder(){
+        return LoginData.builder()
+                .email("shweta1@gmail.com")
+                .password("Password@123")
+                .build();
+    }
+
+    private RegisterCustomerData registerCustomerDataBuilder(){
+        return RegisterCustomerData.builder()
+                .email("shweta@gmail.com")
+                .password("Password123")
+                .passwordRepeat("Password123")
+                .securityQuestion(SecurityQuestion.builder()
+                        .updatedAt("2021-03-09T03:30:41.854Z")
+                        .createdAt("2021-03-09T03:30:41.854Z")
+                        .question("Your eldest siblings middle name?")
+                        .build())
+                .securityAnswer("xyz").build();
+    }
+
 }
